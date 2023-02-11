3d105e2f340f804fc1f8d49ddcd9c216d00b910c
Martin <martinjonsson01@gmail.com>
2023-02-11 14:42:59 +0100

    Create prototype scheduler boilerplate

---
+++ crates/ecs/src/lib.rs
@@ -0,0 +1,27 @@
+//! A barebones ECS-implementation for the scheduler prototype.
+
+// rustc lints
+#![warn(
+    let_underscore,
+    nonstandard_style,
+    unused,
+    explicit_outlives_requirements,
+    meta_variable_misuse,
+    missing_debug_implementations,
+    missing_docs,
+    non_ascii_idents,
+    noop_method_call,
+    pointer_structural_match,
+    trivial_casts,
+    trivial_numeric_casts
+)]
+// clippy lints
+#![warn(
+    clippy::cognitive_complexity,
+    clippy::dbg_macro,
+    clippy::if_then_some_else_none,
+    clippy::print_stdout,
+    clippy::rc_mutex,
+    clippy::unwrap_used,
+    clippy::large_enum_variant
+)]
