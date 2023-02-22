f9a544f1276e9de91ddb1913a2f462e5adf1a531
linnosterlund <linnmari@student.chalmers.se>
2023-02-22 16:00:57 +0100

    followed entity-component-scrapyard archetype implementation

---
+++ crates/component_storage/src/entity.rs
@@ -0,0 +1,5 @@
+
+pub struct Entity {
+    pub entity_id: usize,
+    pub generation: usize,
+}
--- crates/component_storage/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod sparse_set;
-mod archetype;
-
-fn main() {
-    print!("hello");
-
-    // let s = sparse_set::SparseSet::new();
-}
