5644832c83090e8df6fac2b5f55612f55614a6fb
Martin <martinjonsson01@gmail.com>
2023-02-22 17:27:05 +0100

    Shut down all worker threads when shutdown_receiver is dropped

--- crates/ecs/src/pool.rs
+++ crates/ecs/src/pool.rs
@@ -1,11 +1,11 @@
 use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
 use std::thread::{Scope, ScopedJoinHandle};
-use std::time::Duration;
 use std::{iter, thread};
 
 use crossbeam::channel::{Receiver, TryRecvError};
 use crossbeam::deque::{Injector, Stealer, Worker};
+use crossbeam::sync::{Parker, Unparker};
 
 use crate::{Schedule, System, World};
 
@@ -48,6 +48,7 @@ impl<'a> Task<'a> {
 #[derive(Debug, Default)]
 pub struct ThreadPool<'a> {
     injector: Arc<Injector<Task<'a>>>,
+    worker_unparkers: Vec<Unparker>,
 }
 
 #[macro_export]
@@ -68,6 +69,21 @@ macro_rules! thread_println {
 
         std::io::Write::flush(&mut stdout)
             .expect("there shouldn't be any I/O errors when writing normal text");
+
+        drop(stdout);
+    }
+}
+
+impl<'a> ThreadPool<'a> {
+    fn add_task(&mut self, task: Task<'a>) {
+        self.injector.push(task);
+        self.notify_all_workers();
+    }
+
+    fn notify_all_workers(&self) {
+        for unparker in &self.worker_unparkers {
+            unparker.unpark();
+        }
     }
 }
 
@@ -82,7 +98,17 @@ impl<'a> Schedule<'a> for ThreadPool<'a> {
             let workers: Vec<_> = (0..num_cpus::get())
                 .map(|_thread_number| {
                     let injector = Arc::clone(&self.injector);
-                    WorkerThread::start(scope, shutdown_receiver.clone(), injector, vec![])
+                    let parker = Parker::new();
+                    let unparker = parker.unparker().clone();
+                    self.worker_unparkers.push(unparker);
+
+                    WorkerThread::start(
+                        scope,
+                        shutdown_receiver.clone(),
+                        parker,
+                        injector,
+                        vec![], // todo: inform threads about each other
+                    )
                 })
                 .collect();
 
@@ -91,8 +117,13 @@ impl<'a> Schedule<'a> for ThreadPool<'a> {
                     thread_println!("working on {:?} and {:?}", system, world);
                     system.run(world);
                 };
-                self.injector.push(Task::new(task));
+                self.add_task(Task::new(task));
             }
+
+            // Block until shutdown command received (or shutdown_receiver dropped).
+            let _ = shutdown_receiver.recv();
+            // Wake up any sleeping workers so they can shut down.
+            self.notify_all_workers();
 
             for worker in workers {
                 let id = worker.thread().id();
@@ -117,6 +148,7 @@ trait Peasant {
 #[derive(Debug)]
 struct WorkerThread<'env> {
     shutdown_receiver: Receiver<()>,
+    parker: Parker,
     local_queue: Worker<Task<'env>>,
     global_queue: Arc<Injector<Task<'env>>>,
     stealers: Vec<Stealer<Task<'env>>>,
@@ -126,15 +158,24 @@ impl<'scope, 'env: 'scope> WorkerThread<'env> {
     fn start(
         scope: &'scope Scope<'scope, '_>,
         shutdown_receiver: Receiver<()>,
+        parker: Parker,
         global_queue: Arc<Injector<Task<'env>>>,
         stealers: Vec<Stealer<Task<'env>>>,
     ) -> ScopedJoinHandle<'scope, ()> {
-        Self::start_with_tasks(scope, shutdown_receiver, global_queue, stealers, vec![])
+        Self::start_with_tasks(
+            scope,
+            shutdown_receiver,
+            parker,
+            global_queue,
+            stealers,
+            vec![],
+        )
     }
 
     fn start_with_tasks(
         scope: &'scope Scope<'scope, '_>,
         shutdown_receiver: Receiver<()>,
+        parker: Parker,
         global_queue: Arc<Injector<Task<'env>>>,
         stealers: Vec<Stealer<Task<'env>>>,
         tasks: impl IntoIterator<Item = Task<'env>> + Send + 'scope,
@@ -142,6 +183,7 @@ impl<'scope, 'env: 'scope> WorkerThread<'env> {
         scope.spawn(move || {
             let worker = Self {
                 shutdown_receiver,
+                parker,
                 local_queue: Worker::new_fifo(),
                 global_queue,
                 stealers,
@@ -160,9 +202,8 @@ impl<'scope, 'env: 'scope> WorkerThread<'env> {
                 thread_println!("running task...");
                 task.run();
             } else {
-                thread_println!("found no task");
-                // todo: put thread to sleep until new tasks arrive - don't just sleep for 10ms
-                thread::sleep(Duration::from_millis(10));
+                thread_println!("found no task, going to sleep...");
+                self.parker.park();
             }
         }
         thread_println!("exited due to shutdown command!");
@@ -190,6 +231,7 @@ impl<'env> Peasant for WorkerThread<'env> {
 mod tests {
     use std::sync::Arc;
     use std::thread;
+    use std::time::Duration;
 
     use crossbeam::atomic::AtomicCell;
     use crossbeam::channel::{bounded, unbounded};
@@ -222,6 +264,7 @@ mod tests {
         let (_, shutdown_receiver) = bounded(1);
         WorkerThread {
             shutdown_receiver,
+            parker: Default::default(),
             local_queue: Worker::new_fifo(),
             global_queue: Arc::new(global_queue),
             stealers,
@@ -271,6 +314,79 @@ mod tests {
         }
     }
 
+    fn spawn_worker_and_wait_for_task_completion(task_functions: Vec<impl FnMut() + Send>) {
+        let delayed_functions: Vec<fn()> = vec![];
+        spawn_worker_and_wait_for_task_completion_with_delay(task_functions, delayed_functions);
+    }
+
+    fn spawn_worker_and_wait_for_task_completion_with_delay(
+        task_functions: Vec<impl FnMut() + Send>,
+        delayed_task_functions: Vec<impl FnMut() + Send>,
+    ) {
+        let task_count = task_functions.len();
+        let mut tasks = vec![];
+        let mut task_parkers = vec![];
+        let mut have_tasks_run = vec![];
+
+        for mut task_function in task_functions {
+            let has_task_run = Arc::new(AtomicCell::new(false));
+            let has_task_run_clone = Arc::clone(&has_task_run);
+
+            let task_parker = Parker::new();
+            let task_unparker = task_parker.unparker().clone();
+
+            let task = Task::new(move || {
+                task_function();
+                has_task_run_clone.store(true);
+                task_unparker.unpark();
+            });
+
+            tasks.push(task);
+            task_parkers.push(task_parker);
+            have_tasks_run.push(has_task_run);
+        }
+
+        let worker_parker = Parker::new();
+        let worker_unparker0 = worker_parker.unparker().clone();
+        let worker_unparker1 = worker_parker.unparker().clone();
+
+        thread::scope(|scope| {
+            let (shutdown_sender, shutdown_receiver) = bounded(1);
+            let global_queue = mock_global_queue();
+            WorkerThread::start_with_tasks(
+                scope,
+                shutdown_receiver,
+                worker_parker,
+                Arc::clone(&global_queue),
+                vec![],
+                tasks,
+            );
+
+            scope.spawn(move || {
+                thread::sleep(Duration::from_millis(10));
+                thread_println!("Pushing delayed tasks!");
+                for delayed_task_function in delayed_task_functions {
+                    global_queue.push(Task::new(delayed_task_function));
+                    worker_unparker0.unpark();
+                }
+            });
+
+            // Wait for all tasks to complete
+            for task_parker in task_parkers {
+                task_parker.park_timeout(Duration::from_secs(1));
+            }
+            drop(shutdown_sender);
+            worker_unparker1.unpark(); // Wake worker up so it can shut down.
+
+            let all_tasks_ran = vec![true].repeat(task_count);
+            let have_tasks_run: Vec<_> = have_tasks_run
+                .iter()
+                .map(|has_run| has_run.take())
+                .collect();
+            assert_eq!(all_tasks_ran, have_tasks_run, "all tasks should run")
+        })
+    }
+
     #[test]
     fn worker_takes_task_from_local_queue_first() {
         let MockedSystem {
@@ -312,175 +428,93 @@ mod tests {
 
     #[test]
     fn worker_runs_given_task() {
-        let parker = Parker::new();
-        let unparker = parker.unparker().clone();
-
-        let has_task_run = Arc::new(AtomicCell::new(false));
-        let has_task_run_clone = Arc::clone(&has_task_run);
-        let task = Task::new(|| {
+        let task = || {
             thread_println!("task executing");
-            has_task_run_clone.store(true);
-            unparker.unpark();
-        });
+        };
 
-        thread::scope(|scope| {
-            let (shutdown_sender, shutdown_receiver) = bounded(1);
-            WorkerThread::start_with_tasks(
-                scope,
-                shutdown_receiver,
-                mock_global_queue(),
-                vec![],
-                vec![task],
-            );
-            // Wait for task to complete
-            parker.park_timeout(Duration::from_secs(1));
-            drop(shutdown_sender);
-
-            assert!(has_task_run.take())
-        })
+        spawn_worker_and_wait_for_task_completion(vec![task]);
     }
 
     #[test]
     fn worker_runs_on_another_thread() {
-        let parker = Parker::new();
-        let unparker = parker.unparker().clone();
-
         let worker_thread_id = Arc::new(AtomicCell::new(None));
         let worker_thread_id_clone = Arc::clone(&worker_thread_id);
-        let task = Task::new(|| {
-            thread_println!("task 12");
+
+        let task = || {
+            thread_println!("task executing");
             worker_thread_id_clone.store(Some(thread::current().id()));
-            unparker.unpark();
-        });
+        };
 
-        thread::scope(|scope| {
-            let (shutdown_sender, shutdown_receiver) = bounded(1);
-            WorkerThread::start_with_tasks(
-                scope,
-                shutdown_receiver,
-                mock_global_queue(),
-                vec![],
-                vec![task],
-            );
-            // Wait for task to complete
-            parker.park_timeout(Duration::from_secs(1));
-            drop(shutdown_sender);
+        spawn_worker_and_wait_for_task_completion(vec![task]);
 
-            let main_thread_id = thread::current().id();
-            assert_ne!(
-                Some(main_thread_id),
-                worker_thread_id.take(),
-                "worker shouldn't run on main thread"
-            )
-        })
+        let main_thread_id = thread::current().id();
+        assert_ne!(
+            Some(main_thread_id),
+            worker_thread_id.take(),
+            "worker shouldn't run on main thread"
+        )
     }
 
     #[test]
     fn worker_runs_multiple_tasks() {
-        let parker = Parker::new();
-        let unparker = Arc::new(parker.unparker().clone());
-
-        let mut tasks = vec![];
-        let mut have_tasks_run = vec![];
-
-        let task_count = 2;
-        for i in 0..task_count {
-            let has_task_run = Arc::new(AtomicCell::new(false));
-            let has_task_run_clone = Arc::clone(&has_task_run);
-            let unparker_clone = Arc::clone(&unparker);
-
-            let task = Task::new(move || {
-                thread_println!("{i}");
-                has_task_run_clone.store(true);
-                // Last task to run unparks
-                if i == task_count - 1 {
-                    unparker_clone.unpark();
+        let tasks = (1..=3)
+            .map(|i| {
+                move || {
+                    thread_println!("task {i} executing");
                 }
-            });
+            })
+            .collect();
 
-            tasks.push(task);
-            have_tasks_run.push(has_task_run);
-        }
-
-        thread::scope(|scope| {
-            let (shutdown_sender, shutdown_receiver) = bounded(1);
-            WorkerThread::start_with_tasks(
-                scope,
-                shutdown_receiver,
-                mock_global_queue(),
-                vec![],
-                tasks,
-            );
-            // Wait for last task to complete
-            parker.park_timeout(Duration::from_secs(1));
-            drop(shutdown_sender);
-
-            let all_tasks_ran = vec![true].repeat(task_count as usize);
-            let have_tasks_run: Vec<_> = have_tasks_run
-                .iter()
-                .map(|has_run| has_run.take())
-                .collect();
-            assert_eq!(all_tasks_ran, have_tasks_run, "all tasks should run")
-        })
+        spawn_worker_and_wait_for_task_completion(tasks);
     }
 
     #[test]
     fn worker_runs_tasks_on_same_thread() {
-        let parker = Parker::new();
-        let unparker = Arc::new(parker.unparker().clone());
-
-        let mut tasks = vec![];
         let mut thread_ids = vec![];
-        let task_count = 2;
-        for i in 0..task_count {
-            let worker_thread_id = Arc::new(AtomicCell::new(None));
-            let worker_thread_id_clone = Arc::clone(&worker_thread_id);
-            let unparker_clone = Arc::clone(&unparker);
 
-            let task = Task::new(move || {
-                let id = thread::current().id();
-                thread_println!("thread {id:?}: {i}");
-                worker_thread_id_clone.store(Some(id));
-                // Last task to run unparks
-                if i == task_count - 1 {
-                    unparker_clone.unpark();
+        let tasks = (1..=3)
+            .map(|i| {
+                let worker_thread_id = Arc::new(AtomicCell::new(None));
+                let worker_thread_id_clone = Arc::clone(&worker_thread_id);
+                thread_ids.push(Arc::clone(&worker_thread_id));
+
+                move || {
+                    let id = thread::current().id();
+                    thread_println!("thread {id:?}: {i}");
+                    worker_thread_id_clone.store(Some(id));
                 }
-            });
-            tasks.push(task);
-            thread_ids.push(Arc::clone(&worker_thread_id));
-        }
+            })
+            .collect();
 
-        thread::scope(|scope| {
-            let (shutdown_sender, shutdown_receiver) = bounded(1);
-            WorkerThread::start_with_tasks(
-                scope,
-                shutdown_receiver,
-                mock_global_queue(),
-                vec![],
-                tasks,
+        spawn_worker_and_wait_for_task_completion(tasks);
+
+        let main_thread_id = thread::current().id();
+        let thread_ids: Vec<_> = thread_ids.iter().map(|id| id.take()).collect();
+        let product = thread_ids
+            .clone()
+            .into_iter()
+            .cartesian_product(thread_ids.into_iter());
+        for (thread_id, other_thread_id) in product {
+            assert_ne!(
+                Some(main_thread_id),
+                thread_id,
+                "worker shouldn't run on main thread"
             );
-            // Wait for last task to complete
-            parker.park_timeout(Duration::from_secs(1));
-            drop(shutdown_sender);
+            assert_eq!(
+                thread_id, other_thread_id,
+                "worker should run all its tasks in same thread"
+            );
+        }
+    }
 
-            let main_thread_id = thread::current().id();
-            let thread_ids: Vec<_> = thread_ids.iter().map(|id| id.take()).collect();
-            let product = thread_ids
-                .clone()
-                .into_iter()
-                .cartesian_product(thread_ids.into_iter());
-            for (thread_id, other_thread_id) in product {
-                assert_ne!(
-                    Some(main_thread_id),
-                    thread_id,
-                    "worker shouldn't run on main thread"
-                );
-                assert_eq!(
-                    thread_id, other_thread_id,
-                    "worker should run all its tasks in same thread"
-                );
-            }
-        })
+    #[test]
+    fn worker_wakes_up_and_executes_delayed_task() {
+        let task_functions: Vec<fn()> = vec![];
+        let delayed_task = || {
+            thread_println!("delayed task is running");
+        };
+
+        spawn_worker_and_wait_for_task_completion_with_delay(task_functions, vec![delayed_task]);
     }
 
     #[test]
@@ -488,21 +522,22 @@ mod tests {
         #[derive(Debug)]
         struct TestComponent(i32);
 
+        let parker = Parker::new();
+        let unparker = parker.unparker().clone();
+
         let (shutdown_sender, shutdown_receiver) = unbounded();
+        let shutdown_thread = thread::spawn(move || {
+            thread_println!("Parking shutdown thread...");
+            parker.park();
+            thread_println!("Shutting down");
+            drop(shutdown_sender);
+        });
+
         let verify_run_system = move || {
             thread_println!("verify_run_system has run!");
 
             // Shut down application once this has run.
-            for _ in 0..num_cpus::get() {
-                match shutdown_sender.try_send(()) {
-                    Ok(_) => {
-                        thread_println!("sent shut down!");
-                    }
-                    Err(e) => {
-                        thread_println!("could not send shutdown because of {e:?}");
-                    }
-                }
-            }
+            unparker.unpark();
         };
 
         fn system_with_read_and_write(health: Write<TestComponent>) {
@@ -525,5 +560,6 @@ mod tests {
 
         let scheduler = ThreadPool::default();
         application.run(scheduler, shutdown_receiver);
+        shutdown_thread.join().unwrap();
     }
 }
