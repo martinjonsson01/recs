64751dca46e96cc44474288fb7c74ada20d5d35b
Jacob Bredin <jacob@bredin.se>
2023-02-23 14:43:12 +0100

    refactor archetype and storage
    Co-authored-by: linnosterlund <linnosterlund@users.noreply.github.com>

--- crates/component_storage/src/archetype_storage.rs
+++ crates/component_storage/src/archetype_storage.rs
@@ -1,39 +1,58 @@
 
 use std::{ops::{Index, IndexMut}, any::{Any, TypeId}, collections::{HashMap}, rc, hash::Hash};
 
-trait ComponentColumn: Any {
-    fn as_any(&self) -> &dyn Any;
-    fn as_any_mut(&mut self) -> &mut dyn Any;
+// trait ComponentColumn: Any {
+//     fn as_any(&self) -> &dyn Any;
+//     fn as_any_mut(&mut self) -> &mut dyn Any;
+// }
+
+// impl<T: 'static> ComponentColumn for Vec<T> {
+//     fn as_any(&self) -> &dyn Any {
+//         self
+//     }
+//     fn as_any_mut(&mut self) -> &mut dyn Any {
+//         self
+//     }
+// }
+
+type ComponentId = usize;
+type ArchetypeId = Vec<TypeId>;
+
+pub struct Storage {
+    // edges_added: HashMap<TypeId, Option<Box<Archetype>>>,
+    // edges_removed: HashMap<TypeId, Option<Box<Archetype>>>,
+    component_type_id_to_component_id: HashMap<TypeId, ComponentId>,
+    entity_id_to_archetype_id: HashMap<EntityId, ArchetypeId>,
+    archetype_id_to_archetype: HashMap<ArchetypeId, Archetype>,
+    // columns: Archetype,
 }
 
-impl<T: 'static> ComponentColumn for Vec<T> {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}
+impl Storage {
+    // fn new() -> Self {
+    //     // Self {
+    //     //     archetype_id: 0,
+    //     //     columns: Archetype::new(),
+    //     //     edges_added: HashMap::new(),
+    //     //     edges_removed: HashMap::new(),
+    //     // }
+    // }
 
-pub struct Archetype {
-    archetype_id: usize,
-    columns: ArchetypeStorage,
-    edges_added: HashMap<TypeId, Option<Box<Archetype>>>,
-    edges_removed: HashMap<TypeId, Option<Box<Archetype>>>,
-}
+    fn add<T: 'static>(&mut self, entity_id: usize, component : T) {
+        if let Some(archetype_id) = self.entity_id_to_archetype_id.get(&entity_id) {
+            let mut new_archetype_id = archetype_id.clone();
+            new_archetype_id.push(TypeId::of::<T>());
+            new_archetype_id.sort();
+            if let Some(found_archetype) = self.archetype_id_to_archetype.get_mut(&new_archetype_id) {
+                found_archetype.insert::<T>(entity_id, component);
+            } else {
+                //create new archetype
+                let new_archetype = Archetype::new(new_archetype_id);
+            }
 
-impl Archetype {
-    fn new() -> Self {
-        Self {
-            archetype_id: 0,
-            columns: ArchetypeStorage::new(),
-            edges_added: HashMap::new(),
-            edges_removed: HashMap::new(),
+            // if let Some(archetype) = self.archetype_id_to_archetype.get_mut(archetype_id) {
+            //     archetype.insert(entity_id, component);
+            // }
         }
-    }
-
-    fn add<T>(&mut self, entity_id: usize) {
-        
     }
 
     fn remove<T>(&mut self, entity_id: usize) {
@@ -48,35 +67,28 @@ impl Archetype {
         
     }
 
-
 }
 
-pub struct ArchetypeStorage {
+// type ComponentId = TypeId;
+type EntityId = usize;
+type ComponentIndex = usize;
+type ComponentColumn = Box<dyn Any>;
+
+pub struct Archetype {
+    archetype_id: ArchetypeId,
     // entities: HashSet<usize>,
-    entity_to_component_index: HashMap<usize, usize>,
+    entity_to_component_index: HashMap<EntityId, ComponentIndex>,
     // components: HashSet<TypeId>,
-    component_id_to_component_vector: HashMap<TypeId, Box<dyn Any>>,
+    component_id_to_component_vector: HashMap<TypeId, ComponentColumn>,
 }
 
-pub trait StorageInterface {
-    fn new();
-    fn insert<T>(&mut self, entity_id: usize, component : T);
-}
-
-impl ArchetypeStorage {
-    fn new() -> Self {
+impl Archetype {
+    fn new(archetype_id: ArchetypeId) -> Self {
         Self {
+            archetype_id: archetype_id,
             entity_to_component_index: HashMap::new(),
             component_id_to_component_vector: HashMap::new(),
         }
-    }
-
-    fn generate_archetype_id(&self) {
-        let keys = self.component_id_to_component_vector.keys();
-        // .fold("".to_string(), |acc:String, &type_id| acc. type_id)
-
-        // let concated : String = keys.into_iter().fold(0,
-        //                                   |mut i,j| {i + j; i});
     }
 
     fn get_component_vector_mut<T: 'static>(&mut self) -> Option<&mut Vec<T>> {
@@ -95,14 +107,14 @@ impl ArchetypeStorage {
                 let index = component_vec.len(); 
                 component_vec.push(component);
                 self.entity_to_component_index.insert(entity_id, index);
-                // println!("Vec Found!");
             } else {
                 todo!();
             }
         } else {
             self.component_id_to_component_vector.insert(component.type_id(), Box::new(vec![component]));
             self.entity_to_component_index.insert(entity_id, 0);
-            // println!("New vec created, component inserted!");
+            self.archetype_id.push(TypeId::of::<T>());
+            self.archetype_id.sort();
         }
     }
 
@@ -136,16 +148,15 @@ impl ArchetypeStorage {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::{collections::HashMap, any::{Any, TypeId}};
 
-    use super::ArchetypeStorage;
+    use crate::archetype_storage::ArchetypeId;
+
+    use super::Archetype;
     
     #[test]
     fn func() {
-        let mut s = ArchetypeStorage {
-            entity_to_component_index: HashMap::new(),
-            component_id_to_component_vector: HashMap::new(),
-        };
+        let mut s = Archetype::new(vec![TypeId::of::<u32>()]);
 
         s.insert::<u32>(0, 12);
         s.insert::<u32>(1, 12);
@@ -155,20 +166,23 @@ mod tests {
     
     }
 
-    #[test]
-    fn empty_storage_returns_none() {
-        // Arrange
-        let s = ArchetypeStorage::new();
-        // Act
-        let result = s.get_ref::<u32>(23);
-        // Assert
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn empty_storage_returns_none() {
+    //     // Arrange
+    //     let s = Archetype::new();
+    //     // Act
+    //     let result = s.get_ref::<u32>(23);
+    //     // Assert
+    //     assert_eq!(result, None);
+    // }
+
+
 
     #[test]
     fn storage_returns_some_when_value_present() {
         // Arrange
-        let mut s = ArchetypeStorage::new();
+
+        let mut s = Archetype::new(vec![TypeId::of::<u32>()]);
         s.insert::<u32>(23, 23);
         // Act
         let result = s.get_ref::<u32>(23);
@@ -179,13 +193,20 @@ mod tests {
     #[test]
     fn double_insert_results_in_updated_value() {
         // Arrange
-        let mut s = ArchetypeStorage::new();
+        let mut s = Archetype::new(vec![TypeId::of::<f64>()]);
         s.insert::<f64>(12, 15.0);
         // Act
         s.insert::<f64>(12, 20.0);
         let result = s.get_ref::<f64>(12);
         // Assert
         assert_eq!(result, Some(&20.0));
+    }
+
+    #[test]
+    fn typeid_test() {
+        let a = 1;
+        let b = 2.0;
+        eprintln!("a.type_id() = {:?}", a.type_id());
     }
 
     // #[test]
