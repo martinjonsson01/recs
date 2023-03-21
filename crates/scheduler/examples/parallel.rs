56404a424212f9d503e4c9241ab2fdcc98b8a36b
Martin <martinjonsson01@gmail.com>
2023-03-21 14:01:49 +0100

    Remove profiling from parallel-example

--- crates/scheduler/examples/parallel.rs
+++ crates/scheduler/examples/parallel.rs
@@ -10,7 +10,6 @@ use tracing::{error, info, instrument, warn};
 fn main() -> Result<(), Report> {
     let mut app = Application::default()
         .with_tracing()?
-        .with_profiling()?
         .add_system(basic_system)
         .add_system(read_b_system)
         .add_system(write_b_system)
