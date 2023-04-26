7e5dff28959a77c5730d588b44471f579be9e708
Martin <martinjonsson01@gmail.com>
2023-04-26 20:02:45 +0200

    Remove redundant into_iter call

--- crates/ecs_bench_suite/benches/n_body.rs
+++ crates/ecs_bench_suite/benches/n_body.rs
@@ -5,7 +5,7 @@ fn bench_n_body(c: &mut Criterion) {
     let mut group = c.benchmark_group("n_body");
 
     // For convenience, use this range for more thorough benchmarking: (0..10_000).step_by(1_000)
-    for bodies in [100] {
+    for bodies in (0..10_000).step_by(1_000) {
         #[cfg(feature = "bench-all-engines")]
         {
             group.bench_with_input(BenchmarkId::new("bevy", bodies), &bodies, |b, &bodies| {
