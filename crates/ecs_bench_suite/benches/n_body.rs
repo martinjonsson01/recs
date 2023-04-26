9f5f103bb92d73f743ea8e8f50d9bafd7dd591d7
Martin <martinjonsson01@gmail.com>
2023-04-26 18:50:46 +0200

    Conditionally disable bevy n-body bench

--- crates/ecs_bench_suite/benches/n_body.rs
+++ crates/ecs_bench_suite/benches/n_body.rs
@@ -5,12 +5,15 @@ fn bench_n_body(c: &mut Criterion) {
     let mut group = c.benchmark_group("n_body");
 
     for bodies in (0..10_000).step_by(1_000) {
+        #[cfg(feature = "bench-all-engines")]
+        {
+            group.bench_with_input(BenchmarkId::new("bevy", bodies), &bodies, |b, &bodies| {
+                let mut bench = bevy::n_body::Benchmark::new(bodies);
+                b.iter(move || bench.run());
+            });
+        }
         group.bench_with_input(BenchmarkId::new("recs", bodies), &bodies, |b, &bodies| {
             let mut bench = recs::n_body::Benchmark::new(bodies);
-            b.iter(move || bench.run());
-        });
-        group.bench_with_input(BenchmarkId::new("bevy", bodies), &bodies, |b, &bodies| {
-            let mut bench = bevy::n_body::Benchmark::new(bodies);
             b.iter(move || bench.run());
         });
     }
