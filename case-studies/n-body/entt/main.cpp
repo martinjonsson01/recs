ecb051a5d91c8c8908b446127afabfc50aadfcfd
Edvin Nillsson <edvinnil@student.chalmers.se>
2023-04-29 23:06:23 +0200

    Add csv export and exponential benchmarks

--- case-studies/n-body/entt/main.cpp
+++ case-studies/n-body/entt/main.cpp
@@ -2,10 +2,15 @@
 #include <eigen3/Eigen/Eigen>
 #include <entt/entt.hpp>
 #include <execution>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <random>
 
 const float FIXED_TIME_STEP = 1.0 / 20000.0;
+const int SAMPLE_COUNT = 10;
+const int LARGEST_EXPONENT = 14;
 
 struct Position {
     Eigen::Vector3f point;
@@ -56,7 +61,8 @@ void gravity(entt::registry& registry) {
         const auto& position = view.get<const Position>(entity);
         auto& acceleration = view.get<Acceleration>(entity);
 
-        auto acceleration_towards_body = [&position](const Position& body_position, const Mass& body_mass) -> Eigen::Vector3f {
+        auto acceleration_towards_body = [&position](const Position& body_position,
+                                                     const Mass& body_mass) -> Eigen::Vector3f {
             auto to_body = body_position.point - position.point;
             auto distance_squared = to_body.squaredNorm();
 
@@ -85,74 +91,85 @@ void update(entt::registry& registry) {
 }
 
 struct Benchmark {
-  int body_count;
-  int ticks_per_measurement;
-  int measurement_count;
+    int body_count;
+    int ticks_per_sample;
+    std::chrono::duration<double> samples[SAMPLE_COUNT]{};
 
-  Benchmark(int body_count, int simulated_ticks, int measurement_count = 10) : body_count(body_count), measurement_count(measurement_count) {
-      ticks_per_measurement = simulated_ticks / measurement_count;
-  }
+    Benchmark(int body_count, int simulated_ticks) : body_count(body_count) {
+        ticks_per_sample = simulated_ticks / SAMPLE_COUNT;
+    }
 
-  void run() const {
-      entt::registry registry;
+    void run() {
+        entt::registry registry;
 
-      std::default_random_engine rnd;
-      std::uniform_real_distribution<float> radius_distribution(0, 1);
-      std::uniform_real_distribution<float> position_distribution(-5, 5);
+        std::default_random_engine rnd;
+        std::uniform_real_distribution<float> radius_distribution(0, 1);
+        std::uniform_real_distribution<float> position_distribution(-5, 5);
 
-      for (int i = 0; i < body_count; i++) {
-          const auto entity = registry.create();
-          auto radius = radius_distribution(rnd);
-          registry.emplace<Position>(entity, Eigen::Vector3f(position_distribution(rnd), position_distribution(rnd), position_distribution(rnd)));
-          registry.emplace<Velocity>(entity, Eigen::Vector3f::Zero());
-          registry.emplace<Acceleration>(entity, Eigen::Vector3f::Zero());
-          registry.emplace<Mass>(entity, 1e6f * radius * radius * radius);
-      }
-      
-      std::vector<std::chrono::duration<double>> measurements;
+        for (int i = 0; i < body_count; i++) {
+            const auto entity = registry.create();
+            auto radius = radius_distribution(rnd);
+            registry.emplace<Position>(entity, Eigen::Vector3f(position_distribution(rnd), position_distribution(rnd),
+                                                               position_distribution(rnd)));
+            registry.emplace<Velocity>(entity, Eigen::Vector3f::Zero());
+            registry.emplace<Acceleration>(entity, Eigen::Vector3f::Zero());
+            registry.emplace<Mass>(entity, 1e6f * radius * radius * radius);
+        }
 
-      for (int i = 0; i < measurement_count; i++) {
-          auto start = std::chrono::high_resolution_clock::now();
-          for (int j = 0; j < ticks_per_measurement; j++) {
-              update(registry);
-          }
-          auto stop = std::chrono::high_resolution_clock::now();
-          measurements.emplace_back(stop - start);
-          std::cout << "." << std::flush;
-      }
-      
-      double mean = 0;
-      for (auto& measurement : measurements) {
-          mean += measurement.count();
-      }
-      mean /= measurement_count;
-      
-      double variance = 0;
-      for (auto& measurement : measurements) {
-          auto val = measurement.count();
-          variance += (val - mean) * (val - mean);
-      }
-      variance /= measurement_count - 1;
-      auto standard_deviation = sqrt(variance);
-      
-      std::cout << "\rBenchmark result for " << body_count << " bodies over " << ticks_per_measurement * measurement_count << " ticks:" << std::endl;
-      std::cout << "Avg. TPS: " << ticks_per_measurement / mean << " Avg. tick time (ms): " << mean << " Standard deviation (ms): " << standard_deviation << std::endl << std::endl;
-  }
+        for (int i = -1; i < SAMPLE_COUNT; i++) {
+            auto start = std::chrono::high_resolution_clock::now();
+            for (int j = 0; j < ticks_per_sample; j++) {
+                update(registry);
+            }
+            auto stop = std::chrono::high_resolution_clock::now();
+            if (i >= 0) samples[i] = stop - start;
+            std::cout << "." << std::flush;
+        }
+
+        std::cout << "\rFinished benchmark with " << body_count << (body_count > 1 ? " bodies" : " body") << "."
+                  << std::endl;
+    }
 };
 
+struct numpunct : std::numpunct<char> {
+    char do_decimal_point() const override { return ','; }
+};
+
+void save_as_csv(const std::vector<Benchmark>& benchmarks) {
+    std::filesystem::path path = "entt.csv";
+    std::cout << "Writing benchmark results to " << std::filesystem::absolute(path) << "." << std::endl;
+
+    std::ofstream csv;
+    csv.open(path);
+    csv.imbue(std::locale(std::cout.getloc(), new numpunct));
+
+    csv << "body_count,ticks_per_sample";
+    for (int i = 0; i < SAMPLE_COUNT; i++) csv << ",sample_" << i;
+    csv << std::endl;
+
+    for (auto& benchmark : benchmarks) {
+        csv << benchmark.body_count << "," << benchmark.ticks_per_sample;
+        for (auto& sample : benchmark.samples) {
+            csv << ",\"" << std::fixed << std::setprecision(15) << sample.count() / benchmark.ticks_per_sample << '"';
+        }
+        csv << std::endl;
+    }
+
+    csv.close();
+}
+
 int main() {
-    Benchmark benchmarks[] = {
-        Benchmark(100, 100000),
-        Benchmark(500, 4000),
-        Benchmark(1000, 1000),
-        Benchmark(5000, 400),
-        Benchmark(10000, 100),
-        Benchmark(100000, 10),
-    };
+    std::vector<Benchmark> benchmarks;
+
+    for (int n = 0; n <= LARGEST_EXPONENT; n++) {
+        benchmarks.emplace_back(1 << n, 1000);
+    }
 
     for (auto& benchmark : benchmarks) {
         benchmark.run();
     }
+
+    save_as_csv(benchmarks);
 
     return 0;
 }
