f406ecdb6fc50b418bf0e3174c19dd2e98c79b14
Martin <martinjonsson01@gmail.com>
2023-04-30 17:15:04 +0200

    Fix race condition in test

--- crates/scheduler/src/executor/mod.rs
+++ crates/scheduler/src/executor/mod.rs
@@ -268,6 +268,8 @@ pub fn create_system_task(system_guard: SystemExecutionGuard, world: &Arc<World>
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::mpsc::channel;
+    use test_log::test;
 
     #[test]
     fn task_ids_increase() {
@@ -282,10 +284,17 @@ mod tests {
     #[test]
     #[should_panic(expected = "Panicking in worker thread!")]
     fn propagates_worker_panic_to_main_thread() {
-        let panicking_system = || panic!("Panicking in worker thread!");
+        let (panic_guard_sender, panic_guard_receiver) = channel::<()>();
+        let panicking_system = move || {
+            let _a = panic_guard_sender;
+            panic!("Panicking in worker thread!");
+        };
 
+        // Dropping the pool joins all threads, so any panic will be propagated.
         let mut pool = WorkerPool::default();
         pool.add_task(Task::new(panicking_system));
-        // Dropping the pool joins all threads, so any panic will be propagated.
+
+        // Wait for system to run before exiting...
+        panic_guard_receiver.recv().err().unwrap();
     }
 }
