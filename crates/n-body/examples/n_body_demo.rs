ece8d40cb15f3fa1beb2cf8437a024337343c11f
Martin <martinjonsson01@gmail.com>
2023-05-02 19:37:46 +0200

    Implement collision in n-body demo (not the benchmarks)

--- crates/n-body/examples/n_body_demo.rs
+++ crates/n-body/examples/n_body_demo.rs
@@ -1,15 +1,19 @@
-use cgmath::Deg;
+use cgmath::{Deg, MetricSpace};
 use crossbeam::channel::unbounded;
-use ecs::{Application, ApplicationBuilder, BasicApplicationBuilder};
+use ecs::systems::command_buffers::Commands;
+use ecs::systems::{Query, Read};
+use ecs::{Application, ApplicationBuilder, BasicApplicationBuilder, Entity};
 use gfx_plugin::Graphical;
 use n_body::scenes::{
     create_rendered_planet_entity, create_rendered_sun_entity, SINGLE_HEAVY_BODY_AT_ORIGIN,
     SMALL_HEAVY_CLUSTERS,
 };
-use n_body::{acceleration, gravity, movement, BodySpawner, GenericResult};
+use n_body::{acceleration, gravity, movement, BodySpawner, GenericResult, Position};
 use scheduler::executor::WorkerPool;
 use scheduler::schedule::PrecedenceGraph;
 use tracing::instrument;
+
+const COLLISION_RADIUS: f32 = 0.1;
 
 #[instrument]
 fn main() -> GenericResult<()> {
@@ -35,6 +39,7 @@ fn main() -> GenericResult<()> {
         .add_system(movement)
         .add_system(acceleration)
         .add_system(gravity)
+        .add_system(collision)
         .build()?;
 
     SMALL_HEAVY_CLUSTERS.spawn_bodies(&mut app, create_rendered_planet_entity)?;
@@ -44,4 +49,24 @@ fn main() -> GenericResult<()> {
     app.run::<WorkerPool, PrecedenceGraph>(shutdown_receiver)?;
 
     Ok(())
+}
+
+fn collision(
+    entity: Entity,
+    position: Read<Position>,
+    others_positions: Query<(Entity, Read<Position>)>,
+    commands: Commands,
+) {
+    for (other_entity, other_position) in others_positions {
+        if other_entity == entity {
+            continue;
+        }
+
+        let distance = position.point.distance2(other_position.point);
+
+        if distance < (COLLISION_RADIUS * COLLISION_RADIUS) {
+            commands.remove(entity);
+            break;
+        }
+    }
 }
